b43d847de2e9df7feb2753f4d375d067d78e1327
LiuHaoYu <liu030910@gmail.com>
2023-05-27 14:15:18 +0800

    master test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log("111111111111")
 const pet = {name:'Tom',age:1,sex:'男',hobby:'无'}
 console.log(pet)
 console.log(pet)
-console.log('pet-test')
+console.log('master-test')
 console.log('hot-fix test')
